87cfe830d59a6748f8a2123ace751a460aaaa66b
Eric <35898232+Eric-ciccotti@users.noreply.github.com>
2021-05-08 09:41:35 +0200

    DepenseItem component improvement

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
-import DepenseItem from './components/DepenseItem';
+import DepenseItem from "./components/DepenseItem";
 
 function App() {
+  const depenses = [
+    {
+      id: "e1",
+      titre: "Papier toilette",
+      montant: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      titre: "Télévision",
+      montant: 799.49,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: "e3",
+      titre: "Assurance voiture",
+      montant: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      titre: "Nouveau bureau (Bois deluxe)",
+      montant: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <DepenseItem> </DepenseItem>
+      <DepenseItem
+        titre={depenses[0].titre}
+        montant={depenses[0].montant}
+        date={depenses[0].date}
+      ></DepenseItem>
+      <DepenseItem
+        titre={depenses[1].titre}
+        montant={depenses[1].montant}
+        date={depenses[1].date}
+      ></DepenseItem>
+      <DepenseItem
+        titre={depenses[2].titre}
+        montant={depenses[2].montant}
+        date={depenses[2].date}
+      ></DepenseItem>
     </div>
   );
 }
--- src/components/DepenseItem.js
+++ src/components/DepenseItem.js
@@ -1,12 +1,24 @@
-import './DepenseItem.css';
+import "./DepenseItem.css";
 
-function DepenseItem() {
+function DepenseItem(props) {
+  //   const depenseDate = new Date(2021, 5, 6);
+  //   const depenseTitle = "Assurance Voiture";
+  //   const depenseMontant = 295.65;
+
+  const mois = props.date.toLocaleString("fr", { month: "long" });
+  const jour = props.date.toLocaleString("fr", { day: "2-digit" });
+  const année = props.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>28 Mars 2021</div>
+      <div>
+        <div>{jour}</div>
+        <div>{mois}</div>
+        <div>{année}</div>
+      </div>
       <div className="expense-item__description">
-        <h2>Assurance Voiture</h2>
-        <div className="expense-item__price">295.65€</div>
+        <h2>{props.titre}</h2>
+        <div className="expense-item__price">{props.montant}€</div>
       </div>
     </div>
   );
